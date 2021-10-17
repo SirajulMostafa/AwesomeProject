c84abd614473b2040436601f3d8ee5a472055d73
sirajul Mostafa <sirajulmost@gmail.com>
2021-10-17 22:50:16 +0600

    stack navigation with tow route

--- App.js
+++ App.js
@@ -1,183 +1,56 @@
-import React, {useState} from 'react';
-import {
-  StyleSheet,
-  View,
-  TextInput,
-  Text,
-  Button,
-  TouchableOpacity,
-  Pressable,
-  Alert,
-  ToastAndroid,
-  Modal,
-} from 'react-native';
-import Header from './components/Header';
-import OnPresseableButton from './components/OnPresseableButton';
-// import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
-const App = () => {
-  const [name, setName] = useState('');
-  const [submitted, SetSubmitted] = useState(false);
-  const [modalVisible, setModalVisible] = useState(false);
-  onPressHandler = () => {
-    if (name.length > 3) {
-      SetSubmitted(!submitted);
-      // setModalVisible(false);
-    } else {
-      setModalVisible(true);
-      
-    }
-  };
+import React from 'react';
+import {StyleSheet, Text, View,Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+function HomeScreen({navigation}) {
   return (
-   
-    <View style={styles.centeredView}>
-       <Header/>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <View style={styles.warning_title}>
-              <Text style={styles.text}>WORNING!</Text>
-            </View>
-            <View style={styles.modalBody}>
-              <Text style={styles.modalText}>
-                Please enter more than 3 characters !
-              </Text>
-            </View>
+    <View style={StyleSheet.homescreenView}>
+      <Text>Home Screen</Text>
 
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => setModalVisible(!modalVisible)}
-              //onPress={onPressHandler}
-            >
-              <Text style={styles.textStyle}>Hide Modal</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-
-      
-
-      <Text style={styles.text}>Please Enter Your Name :</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        placeholderTextColor="green"
-        autoCapitalize="none"
-        // keyboardType='numeric'
-        onChangeText={value => {
-          setName(value);
-        }}
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
       />
-
-<OnPresseableButton
-     onPressFunction={onPressHandler}
-     title={submitted ?'clear':'submit'}
-      />
-  
-  <OnPresseableButton
-     onPressFunction={onPressHandler}
-     title={'test'}
-      />
-      {submitted ? (
-        <View style={styles.body}>
-        
-          <Text style={styles.text}>Your are register as {name}.</Text>
-        </View>
-      ) : null}
+    </View>
+  );
+}
+const DetailsScreen = () => {
+  return (
+    <View style={styles.detailsScreen}>
+      <Text>Details Screen</Text>
     </View>
   );
 };
-const styles = StyleSheet.create({
-  warning_title: {
-    alignItems: 'center',
-     justifyContent: 'center',
-    backgroundColor: '#ff0',
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-  },
-  body: {
-    flex: 1,
-    flexDirection: 'column',
-    // justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
-  },
-  modalBody: {
-    alignItems: 'center',
-    backgroundColor: '#ffffff',
-    minHeight: 200,
-  },
-  item: {
-    margin: 10,
-    backgroundColor: '#4ae1fa',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  input: {
-    width: 200,
-    margin: 16,
-    height: 42,
-    borderColor: 'pink',
-    borderWidth: 1,
-  },
-    text: {
-      color: '#000000',
-      fontSize: 30,
-      margin: 5,
-      textAlign: 'center',
-    },
 
-  centeredView: {
-     flex: 1,
-    // justifyContent: 'center',
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Overview'}}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  homescreenView: {
+    flex: 1,
     alignItems: 'center',
-    // marginTop: 22,
+    justifyContent: 'center',
   },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
+  details: {
+    flex: 1,
     alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-    marginBottom: 10,
-  },
-  buttonOpen: {
-    backgroundColor: '#F194FF',
-  },
-  buttonClose: {
-    borderBottomRightRadius: 20,
-    borderBottomLeftRadius: 20,
-    backgroundColor: '#2196F3',
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  modalText: {
-    marginBottom: 10,
-    textAlign: 'center',
+    justifyContent: 'center',
   },
 });
-
 export default App;
