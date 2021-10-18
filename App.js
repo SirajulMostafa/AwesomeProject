729172a53a50e2854f57b3379eeffc038c57c459
sirajul Mostafa <sirajulmost@gmail.com>
2021-10-18 10:14:10 +0600

    pass param on screen to antother screen navigation

--- App.js
+++ App.js
@@ -10,22 +10,39 @@ function HomeScreen({navigation}) {
 
       <Button
         title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+      onPress={() => navigation.navigate('Details',{
+        itemId:86,
+        otherParam:'this is other param ',
+      })}
       />
     </View>
   );
 }
-const DetailsScreen = ({navigation}) => {
+const DetailsScreen = ({route,navigation}) => {
+
+  const {itemId,otherParam} = route.params
+
   return (
     <View style={styles.detailsScreen}>
       <Text>Details Screen</Text>
       <Button
         title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          })
+        }
       />
 
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+      <Text> Itemid: {JSON.stringify(itemId)}</Text>
+      <Text> otherParam: {JSON.stringify(otherParam)}</Text>
+      
     </View>
   );
 };
