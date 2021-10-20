fa86da14063cf0e190b5094b252c24ee96531b12
sirajul Mostafa <sirajulmost@gmail.com>
2021-10-20 19:12:11 +0600

    add tab on stack

--- App.js
+++ App.js
@@ -2,76 +2,68 @@ import React from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome'
-function HomeScreen({ navigation }) {
-  const [count, setCount] = React.useState(0);
-
-  React.useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <Button onPress={() => setCount(c => c + 1)} title="Update count" />
-      ),
-    });
-  }, [navigation]);
-
-  return <Text>Count: {count}</Text>;
-}
-function LogoTitle() {
-  return (
-    <Icon
-      style={{ width: 50, height: 50 }}
-      // source={require('')}p
-      name="home" size={40 }color="#900"
-    />
-  );
-}
-const DetailsScreen = ({route,navigation}) => {
-
-  const {itemId,otherParam} = route.params
-
-  return (
-    <View style={styles.detailsScreen}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() =>
-          navigation.push('Details', {
-            itemId: Math.floor(Math.random() * 100),
-          })
-        }
-      />
-
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button
-        title="Go back to first screen in stack"
-        onPress={() => navigation.popToTop()}
-      />
-      <Text> Itemid: {JSON.stringify(itemId)}</Text>
-      <Text> otherParam: {JSON.stringify(otherParam)}</Text>
-      
-    </View>
-  );
-};
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const Profile =()=>{
+  return(
+    <View>
+      <Text>THis is profile screen </Text>
+    </View>
+  )
+}
+const Settings =()=>{
+  return(
+    <View>
+      <Text>This is Settings screen </Text>
+    </View>
+  )
+}
+
+const Feed =()=>{
+  return(
+    <View>
+      <Text>This is Feed screen </Text>
+    </View>
+  )
+}
+const Messages =()=>{
+  return(
+    <View>
+      <Text>This is Messages  screen </Text>
+    </View>
+  )
+}
+
+
+function Home() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="Messages" component={Messages} />
+    </Tab.Navigator>
+  );
+}
+
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
-        <Stack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={({ navigation, route }) => ({
-          headerTitle: props => <LogoTitle {...props} />,
-        })}
-      />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Profile" component={Profile} />
+          <Stack.Screen name="Settings" component={Settings} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
 };
 
 const styles = StyleSheet.create({
